4214492ea5e48c315296560d02995b0f3e54b70f
Руслан Кадыров <kadirov@hited1.ru>
2016-11-24 13:55:04 +0300

    add Photogallery component

--- src/index.js
+++ src/index.js
@@ -1,18 +1,8 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-import MainLayout from './components/MainLayout';
-import App from './components/App';
-import Profile from './components/Profile';
-import './index.css';
-
-import { Router, Route, IndexRoute ,hashHistory  } from 'react-router'
+import routes from './routes';
+import './styles/index.css';
 
 ReactDOM.render(
-  <Router history={hashHistory}>
-      <Route path="/" component={MainLayout}>
-          <IndexRoute component={App}/>
-          <Route path="/profile" component={Profile} />
-      </Route>
-  </Router>,
+  routes,
   document.getElementById('root')
 );
---
+++ src/routes/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Router, Route, IndexRoute ,browserHistory  } from 'react-router'
+
+import MainLayout from '../components/MainLayout';
+import Home from '../components/Home';
+import Profile from '../components/Profile';
+
+  export default (<Router history={browserHistory}>
+      <Route path="/" component={MainLayout}>
+          <IndexRoute component={Home}/>
+          <Route path="/profile" component={Profile} />
+      </Route>
+  </Router>
+);
